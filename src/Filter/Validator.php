8b182ad9f9b853a79e2d09c3744ad7967c5dbf61
Matthew Weier O'Phinney <matthew@weierophinney.net>
2019-12-31 11:20:10 -0600

    Rewriting as Laminas Project package

--- src/Filter/Validator.php
+++ src/Filter/Validator.php
@@ -1,31 +1,30 @@
 <?php
+
 /**
- * Zend Framework (http://framework.zend.com/)
- *
- * @link      http://github.com/zendframework/zf2 for the canonical source repository
- * @copyright Copyright (c) 2005-2015 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   http://framework.zend.com/license/new-bsd New BSD License
+ * @see       https://github.com/laminas/laminas-log for the canonical source repository
+ * @copyright https://github.com/laminas/laminas-log/blob/master/COPYRIGHT.md
+ * @license   https://github.com/laminas/laminas-log/blob/master/LICENSE.md New BSD License
  */
 
-namespace Zend\Log\Filter;
+namespace Laminas\Log\Filter;
 
+use Laminas\Log\Exception;
+use Laminas\Validator\ValidatorInterface as LaminasValidator;
 use Traversable;
-use Zend\Log\Exception;
-use Zend\Validator\ValidatorInterface as ZendValidator;
 
 class Validator implements FilterInterface
 {
     /**
      * Regex to match
      *
-     * @var ZendValidator
+     * @var LaminasValidator
      */
     protected $validator;
 
     /**
      * Filter out any log messages not matching the validator
      *
-     * @param  ZendValidator|array|Traversable $validator
+     * @param  LaminasValidator|array|Traversable $validator
      * @throws Exception\InvalidArgumentException
      * @return Validator
      */
@@ -37,9 +36,9 @@ class Validator implements FilterInterface
         if (is_array($validator)) {
             $validator = isset($validator['validator']) ? $validator['validator'] : null;
         }
-        if (!$validator instanceof ZendValidator) {
+        if (!$validator instanceof LaminasValidator) {
             throw new Exception\InvalidArgumentException(sprintf(
-                'Parameter of type %s is invalid; must implement Zend\Validator\ValidatorInterface',
+                'Parameter of type %s is invalid; must implement Laminas\Validator\ValidatorInterface',
                 (is_object($validator) ? get_class($validator) : gettype($validator))
             ));
         }
