cf649ea4a9e893051c18b82ab3a871c6195b0157
Matthew Weier O'Phinney <matthew@weierophinney.net>
2019-12-31 11:20:19 -0600

    Rewriting as Laminas Project package

--- test/Filter/ValidatorTest.php
+++ test/Filter/ValidatorTest.php
@@ -1,21 +1,20 @@
 <?php
+
 /**
- * Zend Framework (http://framework.zend.com/)
- *
- * @link      http://github.com/zendframework/zf2 for the canonical source repository
- * @copyright Copyright (c) 2005-2015 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   http://framework.zend.com/license/new-bsd New BSD License
+ * @see       https://github.com/laminas/laminas-log for the canonical source repository
+ * @copyright https://github.com/laminas/laminas-log/blob/master/COPYRIGHT.md
+ * @license   https://github.com/laminas/laminas-log/blob/master/LICENSE.md New BSD License
  */
 
-namespace ZendTest\Log\Filter;
+namespace LaminasTest\Log\Filter;
 
-use Zend\Log\Filter\Validator;
-use Zend\Validator\ValidatorChain;
-use Zend\Validator\Digits as DigitsFilter;
-use Zend\Validator\NotEmpty as NotEmptyFilter;
+use Laminas\Log\Filter\Validator;
+use Laminas\Validator\Digits as DigitsFilter;
+use Laminas\Validator\NotEmpty as NotEmptyFilter;
+use Laminas\Validator\ValidatorChain;
 
 /**
- * @group      Zend_Log
+ * @group      Laminas_Log
  */
 class ValidatorTest extends \PHPUnit_Framework_TestCase
 {
