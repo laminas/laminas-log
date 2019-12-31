1655f32a169ba02a581697644f0a1cb8f74851f5
Matthew Weier O'Phinney <matthew@weierophinney.net>
2019-12-31 11:20:13 -0600

    Rewriting as Laminas Project package

--- test/Filter/SampleTest.php
+++ test/Filter/SampleTest.php
@@ -1,28 +1,26 @@
 <?php
+
 /**
- * Zend Framework (http://framework.zend.com/)
- *
- * @link      http://github.com/zendframework/zf2 for the canonical source repository
- * @copyright Copyright (c) 2005-2015 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   http://framework.zend.com/license/new-bsd New BSD License
- * @package   Zend_Log
+ * @see       https://github.com/laminas/laminas-log for the canonical source repository
+ * @copyright https://github.com/laminas/laminas-log/blob/master/COPYRIGHT.md
+ * @license   https://github.com/laminas/laminas-log/blob/master/LICENSE.md New BSD License
  */
 
-namespace ZendTest\Log\Filter;
+namespace LaminasTest\Log\Filter;
 
-use Zend\Log\Filter\Sample;
+use Laminas\Log\Filter\Sample;
 
 /**
- * @category   Zend
- * @package    Zend_Log
+ * @category   Laminas
+ * @package    Laminas_Log
  * @subpackage UnitTests
- * @group      Zend_Log
+ * @group      Laminas_Log
  */
 class SampleTest extends \PHPUnit_Framework_TestCase
 {
     public function testConstructorThrowsOnInvalidSampleRate()
     {
-        $this->setExpectedException('Zend\Log\Exception\InvalidArgumentException', 'must be numeric');
+        $this->setExpectedException('Laminas\Log\Exception\InvalidArgumentException', 'must be numeric');
         new Sample('bar');
     }
 
