bc10f61110d92685a55d0737cdc7e5f621a29706
Matthew Weier O'Phinney <matthew@weierophinney.net>
2019-12-31 11:18:51 -0600

    Rewriting as Laminas Project package

--- test/Writer/NullTest.php
+++ test/Writer/NullTest.php
@@ -1,22 +1,20 @@
 <?php
+
 /**
- * Zend Framework (http://framework.zend.com/)
- *
- * @link      http://github.com/zendframework/zf2 for the canonical source repository
- * @copyright Copyright (c) 2005-2013 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   http://framework.zend.com/license/new-bsd New BSD License
- * @package   Zend_Log
+ * @see       https://github.com/laminas/laminas-log for the canonical source repository
+ * @copyright https://github.com/laminas/laminas-log/blob/master/COPYRIGHT.md
+ * @license   https://github.com/laminas/laminas-log/blob/master/LICENSE.md New BSD License
  */
 
-namespace ZendTest\Log\Writer;
+namespace LaminasTest\Log\Writer;
 
-use Zend\Log\Writer\Null as NullWriter;
+use Laminas\Log\Writer\Null as NullWriter;
 
 /**
- * @category   Zend
- * @package    Zend_Log
+ * @category   Laminas
+ * @package    Laminas_Log
  * @subpackage UnitTests
- * @group      Zend_Log
+ * @group      Laminas_Log
  */
 class NullTest extends \PHPUnit_Framework_TestCase
 {
